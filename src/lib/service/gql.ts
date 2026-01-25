0bdcff9b5cc29f5b48d9976209fc0c15ccd4753c
Kashish Vashistha <kashish.vashistha1@gmail.com>
2026-01-26 04:13:54 +0530

    corrected bug of prisma

--- src/lib/service/gql.ts
+++ src/lib/service/gql.ts
@@ -1,3 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-export const gqlClient = new GraphQLClient("http://localhost:3000/api/graphql");
+export const gqlClient = new GraphQLClient("/api/graphql", {
+    credentials: "include",
+});
