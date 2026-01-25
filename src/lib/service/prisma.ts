7984193bcdae61a7a3e982d6b7e49f2cdcfa3a26
Kashish Vashistha <kashish.vashistha1@gmail.com>
2026-01-26 03:59:19 +0530

    corrected bug of prisma

--- src/lib/service/prisma.ts
+++ src/lib/service/prisma.ts
@@ -1,4 +1,6 @@
 import "dotenv/config";
 import { PrismaClient } from "../../../generated/prisma"
 
-export const prismaClient = new PrismaClient();
+export const prismaClient = new PrismaClient({
+    datasourceUrl: process.env.MONGODB_URL,
+});
