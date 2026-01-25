38596d435edb5717b2233cb9603ae2a80f8a48e5
Kashish Vashistha <kashish.vashistha1@gmail.com>
2026-01-26 04:18:53 +0530

    corrected bug of prisma

--- src/lib/service/gql.ts
+++ src/lib/service/gql.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-export const gqlClient = new GraphQLClient("/api/graphql", {
+export const gqlClient = new GraphQLClient("https://store-management-pink-one.vercel.app/api/graphql", {
     credentials: "include",
 });
