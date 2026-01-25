2aa3f2dca9293c15f36947ba2e7199cd0b11d8e1
Kashish Vashistha <kashish.vashistha1@gmail.com>
2026-01-26 03:45:58 +0530

    prisma bug

--- src/lib/service/prisma.ts
+++ src/lib/service/prisma.ts
@@ -1,5 +1,4 @@
 import "dotenv/config";
-// import { PrismaClient } from "generated/prisma/client";
 import { PrismaClient } from "../../../generated/prisma"
 
 export const prismaClient = new PrismaClient();
