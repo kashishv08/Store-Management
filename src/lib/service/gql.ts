c100c16ad8ec4d5bf39b9ddbe50996c0a3f562cc
Kashish Vashistha <kashish.vashistha1@gmail.com>
2026-01-26 12:03:43 +0530

    corrected bug of prisma

--- src/lib/service/gql.ts
+++ src/lib/service/gql.ts
@@ -1,8 +1,13 @@
 import { GraphQLClient } from "graphql-request";
 
 const isServer = typeof window === "undefined";
-const baseURL = isServer ? process.env.NEXT_PUBLIC_URL : "";
+const getBaseURL = () => {
+    if (!isServer) return "";
+    if (process.env.NEXT_PUBLIC_URL) return process.env.NEXT_PUBLIC_URL;
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    return "http://localhost:3000";
+};
 
-export const gqlClient = new GraphQLClient((baseURL || "") + "/api/graphql", {
+export const gqlClient = new GraphQLClient(getBaseURL() + "/api/graphql", {
     credentials: "include",
 });
