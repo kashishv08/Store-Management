e5c9a8435e16c238fb8b221a9443de81d83e22f3
Kashish Vashistha <kashish.vashistha1@gmail.com>
2026-01-26 12:17:38 +0530

    corrected bug of prisma

--- src/app/api/graphql/resolver/user.ts
+++ src/app/api/graphql/resolver/user.ts
@@ -36,8 +36,8 @@ export const loginUser = async (
 
       cookie.set("newToken", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         path: "/",
         maxAge: 60 * 60 * 24 * 7,
       });
